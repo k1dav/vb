04bc9a74facf62a5ce03224f8a4a790a9aba2bad
k1dave6412 <k1dave6412@gmail.com>
2016-11-16 16:16:21 +0800

    hw

--- HW2-B10409020-宋豐价/Hw1/Module1.vb
+++ HW2-B10409020-宋豐价/Hw1/Module1.vb
@@ -4,41 +4,53 @@
         Protected _a As Single
         Public Overridable Sub Area(ByVal x As Single)
         End Sub
+        Public Overridable Sub Area(ByVal x As Single, ByVal y As Single)
+        End Sub
     End Class
 
     Class Circle
         Inherits Shape
-        Public Overridable Sub Area(ByVal x As Single)
+        Public Overrides Sub Area(ByVal x As Single)
             _a = x * x * Math.PI
-            Console.WriteLine(_a)
+            Console.WriteLine("面積為：" & _a)
         End Sub
     End Class
 
     Class Triangle
         Inherits Shape
-        Public Overridable Sub Area(ByVal x As Single, ByVal y As Single)
+        Public Overrides Sub Area(ByVal x As Single, ByVal y As Single)
             _a = x * y / 2
-            Console.WriteLine(_a)
+            Console.WriteLine("面積為：" & _a)
         End Sub
     End Class
 
     Class Rectangle
         Inherits Shape
-        Public Overridable Sub Area(ByVal x As Single, ByVal y As Single)
+        Public Overrides Sub Area(ByVal x As Single, ByVal y As Single)
             _a = x * y
-            Console.WriteLine(_a)
+            Console.WriteLine("面積為：" & _a)
         End Sub
     End Class
 
     Sub Main()
         Dim c As New Circle
-        c.Area(1)
+        Console.WriteLine("請輸入圓的半徑")
+        Dim cr As Integer = Integer.Parse(Console.ReadLine())
+        c.Area(cr)
 
         Dim t As New Triangle
-        t.Area(10, 10)
+        Console.WriteLine("請輸入三角形的底")
+        Dim b As Integer = Integer.Parse(Console.ReadLine())
+        Console.WriteLine("請輸入三角形的高")
+        Dim h As Integer = Integer.Parse(Console.ReadLine())
+        t.Area(b, h)
 
         Dim r As New Rectangle
-        r.Area(10, 10)
+        Console.WriteLine("請輸入長方形的長")
+        Dim l As Integer = Integer.Parse(Console.ReadLine())
+        Console.WriteLine("請輸入長方形的寬")
+        Dim w As Integer = Integer.Parse(Console.ReadLine())
+        r.Area(l, w)
 
         Console.Read()
     End Sub
--- HW2-B10409020-宋豐价/Hw2/Module1.vb
+++ HW2-B10409020-宋豐价/Hw2/Module1.vb
@@ -1,25 +1,80 @@
 ﻿Module Module1
     Class Student
-        Shared count As Integer = 0
+        Public Shared count As Integer = 0
         Private _Sid As String
         Private _Name As String
         Private _Hegiht As Integer
         Private _Weight As Integer
+        Private _BMI As Single
+
         Sub SetData(ByVal sid As String, ByVal name As String, ByVal height As Integer, ByVal weight As Integer)
-            _Sid = sid
-            _Name = name
-            _Hegiht = height
-            _Weight = weight
+            Me._Sid = sid
+            Me._Name = name
+            If (height <= 0 Or weight <= 0) Then
+                Throw New SetException()
+            End If
+            Me._Hegiht = height
+            Me._Weight = weight
             count += 1
+            _BMI = weight / ((height / 100) ^ 2)
+            If (Me._BMI < 18.5 Or Me._BMI >= 24) Then
+                Throw New BMIException()
+            End If
         End Sub
+
+        Sub GetData()
+            Console.WriteLine("學號：{0}{1}姓名：{2}{3}身高：{4}{5}體重：{6}{7}BMI：{8}",
+                              Me._Sid, vbTab, Me._Name, vbTab, Me._Hegiht, vbTab, Me._Weight, vbTab, Me._BMI)
+        End Sub
+
     End Class
 
     Class SetException
         Inherits Exception
-
+        Public Overrides Function ToString() As String
+            Return "身高或體重設定錯誤"
+        End Function
     End Class
+
+    Class BMIException
+        Inherits Exception
+        Public Overrides Function ToString() As String
+            Return "BMI未在正常範圍"
+        End Function
+    End Class
+
     Sub Main()
+        Dim sel As Integer
+        Dim students(100) As Student
+        While (sel <> -1)
+            Console.WriteLine("請輸入學號：(輸入-1離開)")
+            sel = Integer.Parse(Console.ReadLine())
+
+            If (sel = -1) Then
+                Exit While
+            End If
+
+            students(Student.count) = New Student()
+            Console.WriteLine("請輸入姓名：(輸入-1離開)")
+            Dim n As String = Console.ReadLine()
+            Console.WriteLine("請輸入身高(cm)：(輸入-1離開)")
+            Dim h As Integer = Integer.Parse(Console.ReadLine())
+            Console.WriteLine("請輸入體重：(輸入-1離開)")
+            Dim w As Integer = Integer.Parse(Console.ReadLine())
+
+            Try
+                students(Student.count).SetData(sel, n, h, w)
+            Catch ex1 As BMIException
+                Console.WriteLine(ex1.ToString())
+            Catch ex2 As SetException
+                Console.WriteLine(ex2.ToString())
+            End Try
+        End While
+
+        For index As Integer = 0 To Student.count - 1
+            students(index).GetData()
+        Next
+        Console.WriteLine("總共{0}位學生", Student.count)
 
     End Sub
-
 End Module
